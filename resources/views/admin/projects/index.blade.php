4cd0224184bc9818afa80a76f70ab79118390d90
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-02-22 18:16:55 +0100

    added working delete button

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -14,13 +14,21 @@
                     <div class="card" style="width: 18rem;">
                         <img src="{{$project['img']}}" class="card-img-top" alt="...">
                         <div class="card-body">
-                          <h5 class="card-title">{{$project['name']}}</h5>
-                          <a class="card-text" href="{{$project['repository_link']}}">Repository link</a>
-                          <p class="card-text">{{$project['description']}}</p>
-                          <p class="card-text">{{$project['date_start']}}</p>
-                          <p class="card-text">{{$project['date_end']}}</p>
-                          <a href="{{route('admin.projects.show', ['project'=>$project->slug])}}" class="btn btn-primary">Details</a>
-                          <a href="{{route('admin.projects.edit', ['project'=>$project->slug])}}" class="btn btn-warning float-end text-white">Edit</a>
+                            <h5 class="card-title">{{$project['name']}}</h5>
+                            <a class="card-text" href="{{$project['repository_link']}}">Repository link</a>
+                            <p class="card-text">{{$project['description']}}</p>
+                            <p class="card-text">{{$project['date_start']}}</p>
+                            <p class="card-text">{{$project['date_end']}}</p>
+                            <div class="col-12 d-flex justify-content-center py-3">
+                                <a href="{{route('admin.projects.show', ['project'=>$project->slug])}}" class="btn btn-primary">Details</a>
+                                <a href="{{route('admin.projects.edit', ['project'=>$project->slug])}}" class="btn btn-warning text-white mx-4">Edit</a>
+                                <form action="{{ route('admin.projects.destroy', $project->slug) }}" method="post" onsubmit="return confirm('Are you sure you want delete this project?')">
+                                    @csrf
+                                    @method('DELETE')
+    
+                                    <button class="btn btn-danger" type="submit">DELETE</button>
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
